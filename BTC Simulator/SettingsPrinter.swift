cc799774b3c6187dccb9703671ef6f33626dff4b
Conor <XS15N1@gmail.com>
2025-01-17 21:57:04 +0000

    Fixed errors.

--- BTC Simulator/SettingsPrinter.swift
+++ BTC Simulator/SettingsPrinter.swift
@@ -10,7 +10,7 @@ import SwiftUI
 extension SimulationSettings {
     func printAllSettings() {
         print("=== FACTOR SETTINGS (once only) ===")
-        print("// DEBUG: SimulationSettings run => periodUnit=\(self.periodUnit.rawValue), userPeriods=\(self.userPeriods), initialBTCPriceUSD=\(self.initialBTCPriceUSD)")
+        print("// DEBUG: SimulationSettings => periodUnit=\(self.periodUnit.rawValue), userPeriods=\(self.userPeriods), initialBTCPriceUSD=\(self.initialBTCPriceUSD)")
         print("// DEBUG:   startingBalance=\(self.startingBalance), averageCostBasis=\(self.averageCostBasis)")
         print("// DEBUG:   lockedRandomSeed=\(self.lockedRandomSeed), seedValue=\(self.seedValue), useRandomSeed=\(self.useRandomSeed)")
         print("// DEBUG:   currencyPreference=\(self.currencyPreference.rawValue)")
@@ -19,32 +19,78 @@ extension SimulationSettings {
         print("// DEBUG: useHistoricalSampling=\(self.useHistoricalSampling)")
         print("// DEBUG: useVolShocks=\(self.useVolShocks)")
         
+        // -----------------------------
         // BULLISH
+        // -----------------------------
         print("// DEBUG: BULLISH FACTORS =>")
-        print("// DEBUG:   useHalving=\(self.useHalving), halvingBump=\(self.halvingBump)")
-        print("// DEBUG:   useInstitutionalDemand=\(self.useInstitutionalDemand), maxDemandBoost=\(self.maxDemandBoost)")
-        print("// DEBUG:   useCountryAdoption=\(self.useCountryAdoption), maxCountryAdBoost=\(self.maxCountryAdBoost)")
-        print("// DEBUG:   useRegulatoryClarity=\(self.useRegulatoryClarity), maxClarityBoost=\(self.maxClarityBoost)")
-        print("// DEBUG:   useEtfApproval=\(self.useEtfApproval), maxEtfBoost=\(self.maxEtfBoost)")
-        print("// DEBUG:   useTechBreakthrough=\(self.useTechBreakthrough), maxTechBoost=\(self.maxTechBoost)")
-        print("// DEBUG:   useScarcityEvents=\(self.useScarcityEvents), maxScarcityBoost=\(self.maxScarcityBoost)")
-        print("// DEBUG:   useGlobalMacroHedge=\(self.useGlobalMacroHedge), maxMacroBoost=\(self.maxMacroBoost)")
-        print("// DEBUG:   useStablecoinShift=\(self.useStablecoinShift), maxStablecoinBoost=\(self.maxStablecoinBoost)")
-        print("// DEBUG:   useDemographicAdoption=\(self.useDemographicAdoption), maxDemoBoost=\(self.maxDemoBoost)")
-        print("// DEBUG:   useAltcoinFlight=\(self.useAltcoinFlight), maxAltcoinBoost=\(self.maxAltcoinBoost)")
-        print("// DEBUG:   useAdoptionFactor=\(self.useAdoptionFactor), adoptionBaseFactor=\(self.adoptionBaseFactor)")
         
+        print("// DEBUG:   useHalving=\(self.useHalving), [Wk=\(self.useHalvingWeekly), Mo=\(self.useHalvingMonthly)]")
+        print("// DEBUG:       halvingBumpWeekly=\(self.halvingBumpWeekly), halvingBumpMonthly=\(self.halvingBumpMonthly)")
+        
+        print("// DEBUG:   useInstitutionalDemand=\(self.useInstitutionalDemand), [Wk=\(self.useInstitutionalDemandWeekly), Mo=\(self.useInstitutionalDemandMonthly)]")
+        print("// DEBUG:       maxDemandBoostWeekly=\(self.maxDemandBoostWeekly), maxDemandBoostMonthly=\(self.maxDemandBoostMonthly)")
+        
+        print("// DEBUG:   useCountryAdoption=\(self.useCountryAdoption), [Wk=\(self.useCountryAdoptionWeekly), Mo=\(self.useCountryAdoptionMonthly)]")
+        print("// DEBUG:       maxCountryAdBoostWeekly=\(self.maxCountryAdBoostWeekly), maxCountryAdBoostMonthly=\(self.maxCountryAdBoostMonthly)")
+        
+        print("// DEBUG:   useRegulatoryClarity=\(self.useRegulatoryClarity), [Wk=\(self.useRegulatoryClarityWeekly), Mo=\(self.useRegulatoryClarityMonthly)]")
+        print("// DEBUG:       maxClarityBoostWeekly=\(self.maxClarityBoostWeekly), maxClarityBoostMonthly=\(self.maxClarityBoostMonthly)")
+        
+        print("// DEBUG:   useEtfApproval=\(self.useEtfApproval), [Wk=\(self.useEtfApprovalWeekly), Mo=\(self.useEtfApprovalMonthly)]")
+        print("// DEBUG:       maxEtfBoostWeekly=\(self.maxEtfBoostWeekly), maxEtfBoostMonthly=\(self.maxEtfBoostMonthly)")
+        
+        print("// DEBUG:   useTechBreakthrough=\(self.useTechBreakthrough), [Wk=\(self.useTechBreakthroughWeekly), Mo=\(self.useTechBreakthroughMonthly)]")
+        print("// DEBUG:       maxTechBoostWeekly=\(self.maxTechBoostWeekly), maxTechBoostMonthly=\(self.maxTechBoostMonthly)")
+        
+        print("// DEBUG:   useScarcityEvents=\(self.useScarcityEvents), [Wk=\(self.useScarcityEventsWeekly), Mo=\(self.useScarcityEventsMonthly)]")
+        print("// DEBUG:       maxScarcityBoostWeekly=\(self.maxScarcityBoostWeekly), maxScarcityBoostMonthly=\(self.maxScarcityBoostMonthly)")
+        
+        print("// DEBUG:   useGlobalMacroHedge=\(self.useGlobalMacroHedge), [Wk=\(self.useGlobalMacroHedgeWeekly), Mo=\(self.useGlobalMacroHedgeMonthly)]")
+        print("// DEBUG:       maxMacroBoostWeekly=\(self.maxMacroBoostWeekly), maxMacroBoostMonthly=\(self.maxMacroBoostMonthly)")
+        
+        print("// DEBUG:   useStablecoinShift=\(self.useStablecoinShift), [Wk=\(self.useStablecoinShiftWeekly), Mo=\(self.useStablecoinShiftMonthly)]")
+        print("// DEBUG:       maxStablecoinBoostWeekly=\(self.maxStablecoinBoostWeekly), maxStablecoinBoostMonthly=\(self.maxStablecoinBoostMonthly)")
+        
+        print("// DEBUG:   useDemographicAdoption=\(self.useDemographicAdoption), [Wk=\(self.useDemographicAdoptionWeekly), Mo=\(self.useDemographicAdoptionMonthly)]")
+        print("// DEBUG:       maxDemoBoostWeekly=\(self.maxDemoBoostWeekly), maxDemoBoostMonthly=\(self.maxDemoBoostMonthly)")
+        
+        print("// DEBUG:   useAltcoinFlight=\(self.useAltcoinFlight), [Wk=\(self.useAltcoinFlightWeekly), Mo=\(self.useAltcoinFlightMonthly)]")
+        print("// DEBUG:       maxAltcoinBoostWeekly=\(self.maxAltcoinBoostWeekly), maxAltcoinBoostMonthly=\(self.maxAltcoinBoostMonthly)")
+        
+        print("// DEBUG:   useAdoptionFactor=\(self.useAdoptionFactor), [Wk=\(self.useAdoptionFactorWeekly), Mo=\(self.useAdoptionFactorMonthly)]")
+        print("// DEBUG:       adoptionBaseFactorWeekly=\(self.adoptionBaseFactorWeekly), adoptionBaseFactorMonthly=\(self.adoptionBaseFactorMonthly)")
+        
+        // -----------------------------
         // BEARISH
+        // -----------------------------
         print("// DEBUG: BEARISH FACTORS =>")
-        print("// DEBUG:   useRegClampdown=\(self.useRegClampdown), maxClampDown=\(self.maxClampDown)")
-        print("// DEBUG:   useCompetitorCoin=\(self.useCompetitorCoin), maxCompetitorBoost=\(self.maxCompetitorBoost)")
-        print("// DEBUG:   useSecurityBreach=\(self.useSecurityBreach), breachImpact=\(self.breachImpact)")
-        print("// DEBUG:   useBubblePop=\(self.useBubblePop), maxPopDrop=\(self.maxPopDrop)")
-        print("// DEBUG:   useStablecoinMeltdown=\(self.useStablecoinMeltdown), maxMeltdownDrop=\(self.maxMeltdownDrop)")
-        print("// DEBUG:   useBlackSwan=\(self.useBlackSwan), blackSwanDrop=\(self.blackSwanDrop)")
-        print("// DEBUG:   useBearMarket=\(self.useBearMarket), bearWeeklyDrift=\(self.bearWeeklyDrift)")
-        print("// DEBUG:   useMaturingMarket=\(self.useMaturingMarket), maxMaturingDrop=\(self.maxMaturingDrop)")
-        print("// DEBUG:   useRecession=\(self.useRecession), maxRecessionDrop=\(self.maxRecessionDrop)")
+        
+        print("// DEBUG:   useRegClampdown=\(self.useRegClampdown), [Wk=\(self.useRegClampdownWeekly), Mo=\(self.useRegClampdownMonthly)]")
+        print("// DEBUG:       maxClampDownWeekly=\(self.maxClampDownWeekly), maxClampDownMonthly=\(self.maxClampDownMonthly)")
+        
+        print("// DEBUG:   useCompetitorCoin=\(self.useCompetitorCoin), [Wk=\(self.useCompetitorCoinWeekly), Mo=\(self.useCompetitorCoinMonthly)]")
+        print("// DEBUG:       maxCompetitorBoostWeekly=\(self.maxCompetitorBoostWeekly), maxCompetitorBoostMonthly=\(self.maxCompetitorBoostMonthly)")
+        
+        print("// DEBUG:   useSecurityBreach=\(self.useSecurityBreach), [Wk=\(self.useSecurityBreachWeekly), Mo=\(self.useSecurityBreachMonthly)]")
+        print("// DEBUG:       breachImpactWeekly=\(self.breachImpactWeekly), breachImpactMonthly=\(self.breachImpactMonthly)")
+        
+        print("// DEBUG:   useBubblePop=\(self.useBubblePop), [Wk=\(self.useBubblePopWeekly), Mo=\(self.useBubblePopMonthly)]")
+        print("// DEBUG:       maxPopDropWeekly=\(self.maxPopDropWeekly), maxPopDropMonthly=\(self.maxPopDropMonthly)")
+        
+        print("// DEBUG:   useStablecoinMeltdown=\(self.useStablecoinMeltdown), [Wk=\(self.useStablecoinMeltdownWeekly), Mo=\(self.useStablecoinMeltdownMonthly)]")
+        print("// DEBUG:       maxMeltdownDropWeekly=\(self.maxMeltdownDropWeekly), maxMeltdownDropMonthly=\(self.maxMeltdownDropMonthly)")
+        
+        print("// DEBUG:   useBlackSwan=\(self.useBlackSwan), [Wk=\(self.useBlackSwanWeekly), Mo=\(self.useBlackSwanMonthly)]")
+        print("// DEBUG:       blackSwanDropWeekly=\(self.blackSwanDropWeekly), blackSwanDropMonthly=\(self.blackSwanDropMonthly)")
+        
+        print("// DEBUG:   useBearMarket=\(self.useBearMarket), [Wk=\(self.useBearMarketWeekly), Mo=\(self.useBearMarketMonthly)]")
+        print("// DEBUG:       bearWeeklyDriftWeekly=\(self.bearWeeklyDriftWeekly), bearWeeklyDriftMonthly=\(self.bearWeeklyDriftMonthly)")
+        
+        print("// DEBUG:   useMaturingMarket=\(self.useMaturingMarket), [Wk=\(self.useMaturingMarketWeekly), Mo=\(self.useMaturingMarketMonthly)]")
+        print("// DEBUG:       maxMaturingDropWeekly=\(self.maxMaturingDropWeekly), maxMaturingDropMonthly=\(self.maxMaturingDropMonthly)")
+        
+        print("// DEBUG:   useRecession=\(self.useRecession), [Wk=\(self.useRecessionWeekly), Mo=\(self.useRecessionMonthly)]")
+        print("// DEBUG:       maxRecessionDropWeekly=\(self.maxRecessionDropWeekly), maxRecessionDropMonthly=\(self.maxRecessionDropMonthly)")
         
         print("// DEBUG: lockHistoricalSampling=\(self.lockHistoricalSampling)")
         print("// DEBUG: toggleAll=\(self.toggleAll), areAllFactorsEnabled=\(self.areAllFactorsEnabled)")
--- BTC Simulator/SimulationSettingsInit.swift
+++ BTC Simulator/SimulationSettingsInit.swift
@@ -69,150 +69,113 @@ extension SimulationSettings {
         if let storedHalving = defaults.object(forKey: "useHalving") as? Bool {
             self.useHalving = storedHalving
         }
-        if defaults.object(forKey: "halvingBump") != nil {
-            self.halvingBump = defaults.double(forKey: "halvingBump")
-        }
-        
+        // Removed: self.halvingBump = ...
+        // If you want to migrate, do so here manually:
+        // if defaults.object(forKey: "halvingBump") != nil {
+        //     let oldVal = defaults.double(forKey: "halvingBump")
+        //     // e.g. halvingBumpWeekly = oldVal
+        // }
+
         if let storedInstitutional = defaults.object(forKey: "useInstitutionalDemand") as? Bool {
             self.useInstitutionalDemand = storedInstitutional
         }
-        if defaults.object(forKey: "maxDemandBoost") != nil {
-            self.maxDemandBoost = defaults.double(forKey: "maxDemandBoost")
-        }
+        // Removed: self.maxDemandBoost = ...
         
         if let storedCountry = defaults.object(forKey: "useCountryAdoption") as? Bool {
             self.useCountryAdoption = storedCountry
         }
-        if defaults.object(forKey: "maxCountryAdBoost") != nil {
-            self.maxCountryAdBoost = defaults.double(forKey: "maxCountryAdBoost")
-        }
+        // Removed: self.maxCountryAdBoost = ...
         
         if let storedRegClarity = defaults.object(forKey: "useRegulatoryClarity") as? Bool {
             self.useRegulatoryClarity = storedRegClarity
         }
-        if defaults.object(forKey: "maxClarityBoost") != nil {
-            self.maxClarityBoost = defaults.double(forKey: "maxClarityBoost")
-        }
+        // Removed: self.maxClarityBoost = ...
         
         if let storedEtf = defaults.object(forKey: "useEtfApproval") as? Bool {
             self.useEtfApproval = storedEtf
         }
-        if defaults.object(forKey: "maxEtfBoost") != nil {
-            self.maxEtfBoost = defaults.double(forKey: "maxEtfBoost")
-        }
+        // Removed: self.maxEtfBoost = ...
         
         if let storedTech = defaults.object(forKey: "useTechBreakthrough") as? Bool {
             self.useTechBreakthrough = storedTech
         }
-        if defaults.object(forKey: "maxTechBoost") != nil {
-            self.maxTechBoost = defaults.double(forKey: "maxTechBoost")
-        }
+        // Removed: self.maxTechBoost = ...
         
         if let storedScarcity = defaults.object(forKey: "useScarcityEvents") as? Bool {
             self.useScarcityEvents = storedScarcity
         }
-        if defaults.object(forKey: "maxScarcityBoost") != nil {
-            self.maxScarcityBoost = defaults.double(forKey: "maxScarcityBoost")
-        }
+        // Removed: self.maxScarcityBoost = ...
         
         if let storedMacro = defaults.object(forKey: "useGlobalMacroHedge") as? Bool {
             self.useGlobalMacroHedge = storedMacro
         }
-        if defaults.object(forKey: "maxMacroBoost") != nil {
-            self.maxMacroBoost = defaults.double(forKey: "maxMacroBoost")
-        }
+        // Removed: self.maxMacroBoost = ...
         
         if let storedStableShift = defaults.object(forKey: "useStablecoinShift") as? Bool {
             self.useStablecoinShift = storedStableShift
         }
-        if defaults.object(forKey: "maxStablecoinBoost") != nil {
-            self.maxStablecoinBoost = defaults.double(forKey: "maxStablecoinBoost")
-        }
+        // Removed: self.maxStablecoinBoost = ...
         
         if let storedDemo = defaults.object(forKey: "useDemographicAdoption") as? Bool {
             self.useDemographicAdoption = storedDemo
         }
-        if defaults.object(forKey: "maxDemoBoost") != nil {
-            self.maxDemoBoost = defaults.double(forKey: "maxDemoBoost")
-        }
+        // Removed: self.maxDemoBoost = ...
         
         if let storedAltcoinFlight = defaults.object(forKey: "useAltcoinFlight") as? Bool {
             self.useAltcoinFlight = storedAltcoinFlight
         }
-        if defaults.object(forKey: "maxAltcoinBoost") != nil {
-            self.maxAltcoinBoost = defaults.double(forKey: "maxAltcoinBoost")
-        }
+        // Removed: self.maxAltcoinBoost = ...
         
         if let storedAdoption = defaults.object(forKey: "useAdoptionFactor") as? Bool {
             self.useAdoptionFactor = storedAdoption
         }
-        if defaults.object(forKey: "adoptionBaseFactor") != nil {
-            self.adoptionBaseFactor = defaults.double(forKey: "adoptionBaseFactor")
-        }
+        // Removed: self.adoptionBaseFactor = ...
         
         // BEARISH FACTORS
         if let storedRegClamp = defaults.object(forKey: "useRegClampdown") as? Bool {
             self.useRegClampdown = storedRegClamp
         }
-        if defaults.object(forKey: "maxClampDown") != nil {
-            self.maxClampDown = defaults.double(forKey: "maxClampDown")
-        }
+        // Removed: self.maxClampDown = ...
         
         if let storedCompetitor = defaults.object(forKey: "useCompetitorCoin") as? Bool {
             self.useCompetitorCoin = storedCompetitor
         }
-        if defaults.object(forKey: "maxCompetitorBoost") != nil {
-            self.maxCompetitorBoost = defaults.double(forKey: "maxCompetitorBoost")
-        }
+        // Removed: self.maxCompetitorBoost = ...
         
         if let storedSecBreach = defaults.object(forKey: "useSecurityBreach") as? Bool {
             self.useSecurityBreach = storedSecBreach
         }
-        if defaults.object(forKey: "breachImpact") != nil {
-            self.breachImpact = defaults.double(forKey: "breachImpact")
-        }
+        // Removed: self.breachImpact = ...
         
         if let storedBubblePop = defaults.object(forKey: "useBubblePop") as? Bool {
             self.useBubblePop = storedBubblePop
         }
-        if defaults.object(forKey: "maxPopDrop") != nil {
-            self.maxPopDrop = defaults.double(forKey: "maxPopDrop")
-        }
+        // Removed: self.maxPopDrop = ...
         
         if let storedStableMeltdown = defaults.object(forKey: "useStablecoinMeltdown") as? Bool {
             self.useStablecoinMeltdown = storedStableMeltdown
         }
-        if defaults.object(forKey: "maxMeltdownDrop") != nil {
-            self.maxMeltdownDrop = defaults.double(forKey: "maxMeltdownDrop")
-        }
+        // Removed: self.maxMeltdownDrop = ...
         
         if let storedSwan = defaults.object(forKey: "useBlackSwan") as? Bool {
             self.useBlackSwan = storedSwan
         }
-        if defaults.object(forKey: "blackSwanDrop") != nil {
-            self.blackSwanDrop = defaults.double(forKey: "blackSwanDrop")
-        }
+        // Removed: self.blackSwanDrop = ...
         
         if let storedBearMkt = defaults.object(forKey: "useBearMarket") as? Bool {
             self.useBearMarket = storedBearMkt
         }
-        if defaults.object(forKey: "bearWeeklyDrift") != nil {
-            self.bearWeeklyDrift = defaults.double(forKey: "bearWeeklyDrift")
-        }
+        // Removed: self.bearWeeklyDrift = ...
         
         if let storedMaturing = defaults.object(forKey: "useMaturingMarket") as? Bool {
             self.useMaturingMarket = storedMaturing
         }
-        if defaults.object(forKey: "maxMaturingDrop") != nil {
-            self.maxMaturingDrop = defaults.double(forKey: "maxMaturingDrop")
-        }
+        // Removed: self.maxMaturingDrop = ...
         
         if let storedRecession = defaults.object(forKey: "useRecession") as? Bool {
             self.useRecession = storedRecession
         }
-        if defaults.object(forKey: "maxRecessionDrop") != nil {
-            self.maxRecessionDrop = defaults.double(forKey: "maxRecessionDrop")
-        }
+        // Removed: self.maxRecessionDrop = ...
         
         // Load our NEW lockHistoricalSampling toggle from defaults
         if let savedLockSampling = defaults.object(forKey: "lockHistoricalSampling") as? Bool {
@@ -221,6 +184,5 @@ extension SimulationSettings {
         
         isInitialized = true
         syncToggleAllState()
-        
     }
 }
